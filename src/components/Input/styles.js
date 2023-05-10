443559d073b5b0f4bab03859d91174336316f997
Henrique Walace <henriquewalace@hotmail.com>
2023-05-10 12:06:26 -0300

    Inicial state with Simple visual

---
+++ src/components/Input/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width:100%;
+    height:75px;
+    background-color:#AAFFAA;
+
+    display:flex;
+    align-items:center;
+    justify-content: flex-end; 
+
+    input {
+        width:100%;
+        height:75px;
+        padding: 0 1rem;
+        border:0;
+        background-color:#AAFFAA;
+
+        font-size:24px;
+        font-family: 'Roboto';
+        text-align:right;
+    }
+`
